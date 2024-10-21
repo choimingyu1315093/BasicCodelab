d54ff7ef1cb0246fdece002d4489f8146519feb7
choimingyu <alsrb1315093@gmail.com>
2024-10-21 10:18:00 +0900

    BasicCodelab 프로젝트 최종

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -17,10 +17,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -145,13 +150,25 @@ fun CardContent(modifier: Modifier = Modifier, name: String){
                 )
             }
         }
-        ElevatedButton(
-            onClick = {
-                expanded = !expanded
-            }
+        IconButton(
+            onClick = {expanded = !expanded}
         ) {
-            Text(if(expanded) stringResource(R.string.show_less) else stringResource(R.string.show_more))
+            Icon(
+                imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if(expanded){
+                    stringResource(R.string.show_less)
+                }else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
+//        ElevatedButton(
+//            onClick = {
+//                expanded = !expanded
+//            }
+//        ) {
+//            Text(if(expanded) stringResource(R.string.show_less) else stringResource(R.string.show_more))
+//        }
     }
 }
 
